7e12cfd2193fb7a2bd0441e700879875e3ba4d09
KevinSinagaa <59599517+KevinSinagaa@users.noreply.github.com>
2020-09-19 11:39:06 +0700

    menambahkan 10 todos allowed

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -15,9 +15,13 @@ const TodoList = () => {
   const [showAdd, setShowAdd] = useState(false);
 
   const addTodo = (value) => {
-    const addedTodo = [...todos, { text: value, isCompleted: false }];
+    if (todos.length < 10) {
+      const addedTodo = [...todos, { text: value, isCompleted: false }];
 
-    setTodos(addedTodo);
+      setTodos(addedTodo);
+    } else {
+      alert("Only 10 todos is allowed!");
+    }
   };
   const completeTodo = (index) => {
     const addedTodo = [...todos];
