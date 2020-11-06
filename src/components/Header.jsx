c4a9369bff7753b433f77f49a12f3f632b2da021
KevinSinagaa <59599517+KevinSinagaa@users.noreply.github.com>
2020-11-07 00:43:53 +0700

    membuat button dengan css module

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Button from "./Button";
+import Button from "./button/Button";
 
 const Header = ({ showAddToggle, showAdd, clearTodos }) => {
   return (
---
+++ src/components/button/Button.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+import cx from "classnames";
+
+import styles from "./button.module.css";
+
+const Button = ({ text, onClick, color, align }) => {
+  // const classNames = [
+  //   "header-btn",
+  //   color === "black" && "main-black-color",
+  //   color === "red" && "main-red-color",
+  //   align === "left" && "align-left",
+  //   align === "right" && "align-right"
+  // ].join(" ");
+
+  const classNames = cx(styles.headerBtn, {
+    [styles.mainBlackColor]: color === "black",
+    [styles.mainRedColor]: color === "red",
+    [styles.alignLeft]: align === "left",
+    [styles.alignRight]: align === "right"
+  });
+
+  return (
+    <button className={classNames} onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  text: "Button",
+  color: "black",
+  align: "left"
+};
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  color: PropTypes.oneOf(["black", "red"]),
+  align: PropTypes.oneOf(["left", "right"])
+};
+
+export default Button;
