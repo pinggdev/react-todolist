a2f769c28ebd155c3a5c8051b096ddede5df2c11
KevinSinagaa <59599517+KevinSinagaa@users.noreply.github.com>
2020-11-07 20:22:46 +0700

    paper component css module

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import Paper from "../components/Paper";
+import Paper from "../components/paper/Paper";
 import Header from "../components/header/Header";
 import TodoForm from "../components/TodoForm";
 import Todos from "../components/Todos";
