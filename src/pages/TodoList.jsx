54ca569370c9428cc615483d454a49b7b157fcfa
KevinSinagaa <59599517+KevinSinagaa@users.noreply.github.com>
2020-11-07 21:03:50 +0700

    todos component css module

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import Paper from "../components/paper/Paper";
 import Header from "../components/header/Header";
 import TodoForm from "../components/todoform/TodoForm";
-import Todos from "../components/Todos";
+import Todos from "../components/todos/Todos";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([
