89964a2286270e342eb1858deb13f1811e5ed8f2
KevinSinagaa <59599517+KevinSinagaa@users.noreply.github.com>
2020-11-07 20:22:29 +0700

    paper component css module

--- src/components/paper/Paper.jsx
+++ src/components/paper/Paper.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import styles from "./paper.module.css";
+
 const Paper = ({ children }) => {
   return (
-    <div className="container">
-      <div className="frame">{children}</div>
+    <div className={styles.container}>
+      <div className={styles.frame}>{children}</div>
     </div>
   );
 };
