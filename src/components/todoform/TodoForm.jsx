6c22bf1143b820e26fe5f1a7f1b869df660bf232
KevinSinagaa <59599517+KevinSinagaa@users.noreply.github.com>
2020-11-07 20:48:56 +0700

    todoform component css module

--- src/components/todoform/TodoForm.jsx
+++ src/components/todoform/TodoForm.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
+import styles from "./todoform.module.css";
+
 const TodoForm = ({ addTodo, showAdd }) => {
   const [value, setValue] = useState("");
 
@@ -24,15 +26,15 @@ const TodoForm = ({ addTodo, showAdd }) => {
 
   if (showAdd) {
     return (
-      <section className="add">
-        <form className="add-form" onSubmit={handleFormSubmit}>
+      <section className={styles.add}>
+        <form className={styles.addForm} onSubmit={handleFormSubmit}>
           <input
             type="text"
-            className="add-input"
+            className={styles.addInput}
             value={value}
             onChange={(e) => setValue(e.target.value)}
           />
-          <button className="add-btn main-black-color">Add</button>
+          <button className={styles.addBtn}>Add</button>
         </form>
       </section>
     );
