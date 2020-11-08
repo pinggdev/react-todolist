124ef088adb45e19a8f77c47bca678f522b0cc8e
KevinSinagaa <59599517+KevinSinagaa@users.noreply.github.com>
2020-11-09 01:53:17 +0700

    emotion ccs in js, setup csb dan cra

---
+++ src/components/button/button.styles.js
@@ -0,0 +1,12 @@
+import { css } from "@emotion/core";
+
+export const button = ({ align }) => css`
+  width: 24%;
+  font-size: 1.8rem;
+  font-family: "Bungee", sans-serif;
+  padding: 16px;
+  background: unset;
+  border: unset;
+  outline: unset;
+  cursor: pointer;
+`;
