58d208867a238c205b6ed96e11710595a1020055
KevinSinagaa <59599517+KevinSinagaa@users.noreply.github.com>
2020-11-09 13:20:13 +0700

    emotion css props di Button component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+/** @jsx jsx */
+// import React from "react";
+import { jsx } from "@emotion/core";
 import PropTypes from "prop-types";
-import cx from "classnames";
+// import cx from "classnames";
 
-import styles from "./button.module.css";
+// import styles from "./button.module.css";
+import * as styles from "./button.styles";
 
 const Button = ({ text, onClick, color, align }) => {
   // const classNames = [
@@ -13,15 +16,18 @@ const Button = ({ text, onClick, color, align }) => {
   //   align === "right" && "align-right"
   // ].join(" ");
 
-  const classNames = cx(styles.headerBtn, {
-    [styles.mainBlackColor]: color === "black",
-    [styles.mainRedColor]: color === "red",
-    [styles.alignLeft]: align === "left",
-    [styles.alignRight]: align === "right"
-  });
+  // const classNames = cx(styles.headerBtn, {
+  //   [styles.mainBlackColor]: color === "black",
+  //   [styles.mainRedColor]: color === "red",
+  //   [styles.alignLeft]: align === "left",
+  //   [styles.alignRight]: align === "right"
+  // });
 
   return (
-    <button className={classNames} onClick={onClick}>
+    // <button className={classNames} onClick={onClick}>
+    //   {text}
+    // </button>
+    <button css={styles.button({ align, color })} onClick={onClick}>
       {text}
     </button>
   );
--- src/components/button/button.styles.js
+++ src/components/button/button.styles.js
@@ -1,12 +1,29 @@
 import { css } from "@emotion/core";
 
-export const button = ({ align }) => css`
-  width: 24%;
-  font-size: 1.8rem;
-  font-family: "Bungee", sans-serif;
-  padding: 16px;
-  background: unset;
-  border: unset;
-  outline: unset;
-  cursor: pointer;
-`;
+export const button = ({ color, align }) => {
+  let textColor;
+
+  switch (color) {
+    case "black":
+      textColor = "#484848";
+      break;
+    case "red":
+      textColor = "#e06262";
+      break;
+    default:
+      textColor = "#484848";
+  }
+
+  return css`
+    font-size: 1.8rem;
+    font-family: "Bungee", sans-serif;
+    color: ${textColor};
+    text-align: ${align};
+    width: 24%;
+    padding: 16px;
+    cursor: pointer;
+    background: unset;
+    border: unset;
+    outline: unset;
+  `;
+};
