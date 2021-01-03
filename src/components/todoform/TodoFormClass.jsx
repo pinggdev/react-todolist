f4a30c96786f816668ef072ab539ad4360427252
KevinSinagaa <59599517+KevinSinagaa@users.noreply.github.com>
2021-01-03 17:00:52 +0700

    class component todoform

---
+++ src/components/todoform/TodoFormClass.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+import { withTheme } from "emotion-theming";
+
+import Button from "../button/ButtonClass";
+import Container from "../../layout/Container";
+import Item from "../../layout/Item";
+
+const StyledInput = styled.input`
+  background: unset;
+  border: unset;
+  padding: 0 64px;
+  width: 100%;
+  border-bottom: 1px solid ${(props) => props.theme.color.primary.red};
+  outline: unset;
+  font-family: "Homemade Apple", sans-serif;
+  font-size: 16px;
+  text-transform: lowercase;
+`;
+
+class TodoForm extends React.Component {
+  state = {
+    value: ""
+  };
+
+  handleFormSubmit = (e) => {
+    const { value } = this.state;
+    const { addTodo } = this.props;
+    e.preventDefault();
+
+    if (!value) {
+      alert("No Blank Todo!");
+      return;
+    }
+
+    if (value.length > 40) {
+      alert("Please create  a sorter todo text!");
+      this.setState({
+        value: ""
+      });
+      return;
+    }
+
+    addTodo(value);
+    this.setState({
+      value: ""
+    });
+  };
+
+  handleOnChange = (e) => {
+    this.setState({
+      value: e.target.value
+    });
+  };
+
+  render() {
+    const { showAdd, theme } = this.props;
+
+    if (showAdd) {
+      return (
+        <section className="todoform-component">
+          <form onSubmit={this.handleFormSubmit}>
+            <Container alignItems="flex-start">
+              <Item flex="1" padding="0 0 0 16px">
+                <StyledInput
+                  theme={theme}
+                  type="text"
+                  value={this.state.value}
+                  onChange={this.handleOnChange}
+                />
+              </Item>
+              <Item>
+                <Button text="Add" />
+              </Item>
+            </Container>
+          </form>
+        </section>
+      );
+    } else {
+      return null;
+    }
+  }
+
+  static propTypes = {
+    addTodo: PropTypes.func.isRequired,
+    showAdd: PropTypes.bool.isRequired
+  };
+}
+
+export default withTheme(TodoForm);
--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import Paper from "../components/paper/PaperClass";
 import Header from "../components/header/HeaderClass";
-import TodoForm from "../components/todoform/TodoForm";
+import TodoForm from "../components/todoform/TodoFormClass";
 import Todos from "../components/todos/Todos";
 
 import Container from "../layout/Container";
